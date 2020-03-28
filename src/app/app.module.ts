26b97f361be239b8b62e16a95c615a48839a468e
vudoan_its <vud@itsj-group.com>
2020-03-28 22:20:42 +0700

    create_weather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    HomeComponent,
+    AboutComponent,
+    FooterComponent,
+    WeatherComponent,
   ],
   imports: [
     BrowserModule,
